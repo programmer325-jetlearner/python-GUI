cb5d9d43f29f6407de3ad5e7b441777a25645f88
programmer325-jetlearner <asyedtanim1@gmail.com>
2026-05-31 11:42:56 +0100

    Add files via upload

--- calender.py
+++ calender.py
@@ -1,6 +1,17 @@
 from tkinter import *
 import calendar
 
+def showcal():
+    new_root=Tk()
+    new_root.config(background="cyan")
+    new_root.title("calender window")
+    new_root.geometry("550x550")
+    fetch_year=int(year_box.get())
+    cal_content=calendar.calendar(fetch_year)
+    cal_year=Label(new_root,text=cal_content,font=("Consolas",10,"bold"))
+    cal_year.grid(row=5,column=1,padx=20)
+    new_root.mainloop()
+
 
 #main window
 root=Tk()
@@ -13,10 +24,14 @@ year_label=Label(root,text="enter year",bg="blue",fg="white",font=("Times",18,"b
 year_label.pack()
 year_box=Entry(root)
 year_box.pack()
-show_btn=Button(root,text="show calendar",bg="green",fg="white",font=("Arial",18,"bold"))
+show_btn=Button(root,text="show calendar",bg="green",fg="white",font=("Arial",18,"bold"),command=showcal)
 show_btn.pack()
-exit_btn=Button(root,text="exit",bg="green",fg="white",font=("Arial",18,"bold"))
+exit_btn=Button(root,text="exit",bg="green",fg="white",font=("Arial",18,"bold"),command=exit)
 exit_btn.pack()
+
+
+
+
 
 
 
