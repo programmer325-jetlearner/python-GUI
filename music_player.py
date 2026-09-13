cafe39121cae779a1902aab838e7e2dab9151e23
programmer325-jetlearner <asyedtanim1@gmail.com>
2026-09-13 02:19:24 -0800

    Add files via upload

--- music_player.py
+++ music_player.py
@@ -4,18 +4,34 @@ from tkinter.filedialog import *
 root=Tk()
 root.configure(background="#989FCE")
 root.title("music player")
-root.geometry("500x500")
+root.geometry("1000x800")
+
+def play():
+    selected_index=listbox.curselection()[0]
+    song_name=songs[selected_index]
+    if song_name:
+        play_lbl.config(text=f"Now playing {song_name}:")
 
 
-play_btn=Button(root,text="PLAY",font=("Arial",40,"bold"),background="#BFCC94",foreground="black")
+
+
+songs=["like a rolling stone","i cant get no satisfaction","imagine","whats going on","respect"]
+
+play_btn=Button(root,text="PLAY",font=("Arial",40,"bold"),background="#BFCC94",foreground="black",command=play)
 play_btn.pack(side=LEFT)
 
 play_lbl=Label(root,text="Now Playing",font=("Arial",30,"bold"),background=("#E6AACE"),foreground="black")
 play_lbl.place(x=150,y=50)
 
-listbox=Listbox(root)
-listbox.pack(side=RIGHT,padx=20)
+scrollbar=Scrollbar(root,orient="vertical")
+scrollbar.pack(side=RIGHT)
 
+listbox=Listbox(root,yscrollcommand=scrollbar.set,background="blue",foreground="yellow")
+listbox.pack(side=RIGHT,padx=20)
+i=0
+for song in songs:
+    listbox.insert(END, song)
+scrollbar.config(command=listbox.yview)
 
 
 
